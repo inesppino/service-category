3560f442e90157185d895081bf30f93cfa312934
inesppino <inesp.pino@gmail.com>
2020-09-03 20:50:08 +0200

    feat(servicecategorylist): agregado el span para las parentesis

--- src/components/ServiceCategoryList.js
+++ src/components/ServiceCategoryList.js
@@ -9,6 +9,13 @@ class ServiceCategoryList extends Component {
         <h3>{titleList}</h3>
       <ul>
         {list.map(category => {
+          if(category.Caption.includes('(')){
+            return (
+            <li key={category.Id}>{category.Caption.split('(')[0]} <span>{'(' + category.Caption.split('(')[1]}</span></li>
+            )
+          } else {
+
+          }
           return (<li key={category.Id}>{category.Caption}</li>)
         })}
       </ul>

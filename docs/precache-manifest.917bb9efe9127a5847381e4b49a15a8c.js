a87b2f5f533474e2ab2e918678e00c7d490a4caf
inesppino <inesp.pino@gmail.com>
2020-09-03 21:06:18 +0200

    docs

---
+++ docs/precache-manifest.917bb9efe9127a5847381e4b49a15a8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f7c311750b918ec9d2afaed9b6a28c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a04908a8669a0202464c",
+    "url": "./static/css/main.a6bda024.chunk.css"
+  },
+  {
+    "revision": "9bfa9d5ca550a566e9b3",
+    "url": "./static/js/2.dc73c52f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.dc73c52f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a04908a8669a0202464c",
+    "url": "./static/js/main.518c638f.chunk.js"
+  },
+  {
+    "revision": "16b7f0075d8ac383988d",
+    "url": "./static/js/runtime-main.779fa9df.js"
+  }
+]);

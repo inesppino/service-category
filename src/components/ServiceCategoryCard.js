4124179e3a7bd17800cbc5db2456605241bc8d6f
inesppino <inesp.pino@gmail.com>
2020-09-05 14:15:42 +0200

    feat: serviceCategoryCard y serviceCategoryList pasado a funciones

--- src/components/ServiceCategoryCard.js
+++ src/components/ServiceCategoryCard.js
@@ -1,26 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import ServiceCategoryList from './ServiceCategoryList';
 
-class ServiceCategoryCard extends Component {
+const ServiceCategoryCard = props => {
+  const { lists, title, handleCollapsible, id, collapsibleControl } = props;
 
-  render() {
-    const { lists, title, handleCollapsible, id, collapsibleControl } = this.props;
-
-    return (
-      <div className={`card-container ${collapsibleControl}`}>
-        <div className="card-title-container">
-          <h2>{title}</h2>
-          <button onClick={() => handleCollapsible(id)}><i className="fas fa-angle-up icon-up"></i></button>
-        </div>
-        <div className="card-lists-container" id={title}>
-          {Object.keys(lists).map((list) => {
-            return (
-              <ServiceCategoryList key={list} titleList={list} list={lists[list]} />
-            );
-          })}
-        </div>
+  return (
+    <div className={`card-container ${collapsibleControl}`}>
+      <div className="card-title-container">
+        <h2>{title}</h2>
+        <button onClick={() => handleCollapsible(id)}><i className="fas fa-angle-up icon-up"></i></button>
       </div>
-    )
-  }
-}
+      <div className="card-lists-container" id={title}>
+        {Object.keys(lists).map((list) => {
+          return (
+            <ServiceCategoryList key={list} titleList={list} list={lists[list]} />
+          );
+        })}
+      </div>
+    </div>
+  )
+};
+
 export default ServiceCategoryCard;
--- src/components/ServiceCategoryList.js
+++ src/components/ServiceCategoryList.js
@@ -1,26 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 
-class ServiceCategoryList extends Component {
-
-  render() {
-    const { list, titleList } = this.props;
-    return (
-      <div>
-        <h3>{titleList}</h3>
-        <ul>
-          {list.map(category => {
-            if (category.Caption.includes('(')) {
-              return (
-                <li key={category.Id}>{category.Caption.split('(')[0]} <span>{'(' + category.Caption.split('(')[1]}</span></li>
-              )
-            } else {
-              return (<li key={category.Id}>{category.Caption}</li>)
-            }
-          })}
-        </ul>
-      </div>
-    )
-  }
-}
+const ServiceCategoryList = props => {
+  const { list, titleList } = props;
+  return (
+    <div>
+      <h3>{titleList}</h3>
+      <ul>
+        {list.map(category => {
+          if (category.Caption.includes('(')) {
+            return (
+              <li key={category.Id}>{category.Caption.split('(')[0]} <span>{'(' + category.Caption.split('(')[1]}</span></li>
+            )
+          } else {
+            return (<li key={category.Id}>{category.Caption}</li>)
+          }
+        })}
+      </ul>
+    </div>
+  )
+};
 
 export default ServiceCategoryList;
